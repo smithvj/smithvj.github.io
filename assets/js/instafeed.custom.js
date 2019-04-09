38d4b8bf920f02ef13094e655407e4c656577273
Vanessa Smith <smithv@fcso.us>
2019-04-09 10:56:15 -0400

    Testing

--- assets/js/instafeed.custom.js
+++ assets/js/instafeed.custom.js
@@ -1,9 +1,15 @@
 // JavaScript source code
-$(document).ready(function () {
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'gofcsonc',
-        clientId: '225a91b9d5fe408b8ec85e82d2371a02'
+$( document ).ready(function() {
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '8660622925',
+        limit: 12,
+        resolution: 'standard_resolution',
+        accessToken: '8660622925.1677ed0.00ecad12adf94e24bc50bf4ede184987',
+        sortBy: 'most-recent',
+        template: '<div class="gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        //   tagName: 'gofcsonc',
+        //   clientId: '225a91b9d5fe408b8ec85e82d2371a02'
     });
-    feed.run();
+    userFeed.run();
 });
