ae09380765a5a4586d462a804d4e131ef3eca4e5
Vanessa Smith <smithv@fcso.us>
2019-04-09 13:53:35 -0400

    Testing

--- assets/js/instafeed.custom.js
+++ assets/js/instafeed.custom.js
@@ -4,10 +4,12 @@ $( document ).ready(function() {
         get: 'user',
         userId: '8660622925',
         limit: 12,
-        resolution: 'standard_resolution',
+        resolution: "standard_resolution": {
+            "orientation": square
+        },
         accessToken: '8660622925.1677ed0.00ecad12adf94e24bc50bf4ede184987',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-3 instagallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class="col-lg-3 instagallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
         //   tagName: 'gofcsonc',
         //   clientId: '225a91b9d5fe408b8ec85e82d2371a02'
     });
