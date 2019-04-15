be2882e65e2f7ff515a110bc6ca1e4c2475e57aa
Vanessa Smith <smithv@fcso.us>
2019-04-15 13:42:21 -0400

    Updates

--- assets/js/instafeed.custom.js
+++ assets/js/instafeed.custom.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         userId: '8660622925',
         limit: 12,
         orientation: 'square',
-        resolution: 'low_resolution',
+        resolution: 'standard_resolution',
         accessToken: '8660622925.1677ed0.00ecad12adf94e24bc50bf4ede184987',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instagallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
