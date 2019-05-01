6f0adc89427ab3242f459131cfc5324fdb1eeb6c
Vanessa Smith <smithv@fcso.us>
2019-05-01 10:46:09 -0400

    Updates to .cs

--- telecommunicator.aspx.cs
+++ telecommunicator.aspx.cs
@@ -5,10 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class MasterPage : System.Web.UI.MasterPage
+public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Page.Header.Title = "FCSO Recruitment Website | " + Page.Title;
+
     }
 }
